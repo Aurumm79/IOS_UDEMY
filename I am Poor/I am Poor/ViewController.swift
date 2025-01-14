c4366a0b3d195abea36f20acdd4bccdd091250ac
Aurumm79 <goldsunje@gmail.com>
2025-01-14 16:43:42 +0900

    Add first project in UDEMY course : I Am Poor

---
+++ I am Poor/I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Sunje Keum on 1/14/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
